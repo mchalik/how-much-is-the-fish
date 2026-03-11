2b19279d962cd466cd51a14d3076daeaa3c858b6
Maxim <maksimchalik@gmail.com>
2026-03-11 14:55:18 +0300

    fixes for vercel

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+
+app.get('/events', (req, res) => {
+    console.log('Клиент подключился :)');
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    
+    req.socket.setTimeout(0);
+
+    res.write('data: Соединение установлено\n\n');
+    let timeout;
+
+
+    const sendPrices = () => {
+        const randomPrice = () => Math.floor(Math.random() * 100) * 0.05 ;
+        const fishPrice = 10 + randomPrice();
+        const chipsPrice = 6 + randomPrice();
+
+        res.write(
+            `event: fishPrice\n` +
+            `data: ${fishPrice.toFixed(2)}\n\n`);
+
+        res.write(
+            `event: chipsPrice\n` +
+            `data: ${chipsPrice.toFixed(2)}\n\n`);
+
+        timeout = setTimeout(sendPrices, Math.random() * 2000 + 1000);
+    }
+
+    sendPrices();
+
+    req.on('close', () => {
+        console.log('Клиент отключился');
+        clearTimeout(timeout);
+        res.end();
+    });
+});
+
+app.get('/', (_req, res) => {
+   res.sendFile(__dirname + '/sse-client.html');
+});
+
+app.listen(3000, () => {
+    console.log('Сервер запущен на порту 3000');
+});
+
+export default app;
